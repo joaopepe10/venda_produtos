24e47cc8554bf78009f23fd38eed03f1a5f3ba4f
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-03 19:57:29 -0300

    Projeto iniciado com gerenciador de dependencias maven, com as seguintes dependencias, Spring Data Jpa, Spring Web, Lombook, Spring DevTool e MySQLConnector

---
+++ src/test/java/com/consumidor/projetosite/ProjetoSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumidor.projetosite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
