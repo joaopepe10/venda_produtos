f9742aacdcc44d2257199c9bd03973984aff5cff
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-03 22:02:44 -0300

    Adicionando primary key nas entidades

--- src/main/java/com/consumidor/projetosite/modelo/Produto.java
+++ src/main/java/com/consumidor/projetosite/modelo/Produto.java
@@ -1,4 +1,25 @@
 package com.consumidor.projetosite.modelo;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @Column(nullable = false)
+    private String nomeProduto;
+
+    @Column(nullable = false)
+    private Float precoProduto;
+
+    @ManyToOne
+    @JoinColumn(name = "id_carrinhp_FK", referencedColumnName = "id_carrinho")
+    private Carrinho carrinho;
+
 }
