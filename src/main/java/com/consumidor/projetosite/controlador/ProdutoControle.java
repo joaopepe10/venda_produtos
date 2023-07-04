167ee27e31b4ac443bf7e50574a7c7e612c75bf1
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-03 22:03:18 -0300

    Criando uma rota para verificacao se a aplicacao esta rodando

--- src/main/java/com/consumidor/projetosite/controlador/ProdutoControle.java
+++ src/main/java/com/consumidor/projetosite/controlador/ProdutoControle.java
@@ -1,4 +1,21 @@
 package com.consumidor.projetosite.controlador;
 
+import com.consumidor.projetosite.modelo.Produto;
+import com.consumidor.projetosite.repositorio.ProdutoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ProdutoControle {
+    @Autowired
+    private ProdutoRepositorio produtoRepositorio;
+
+    @GetMapping("/produto")
+    public Iterable<Produto> listarProdutos(){
+        return produtoRepositorio.findAll();
+    }
 }
