77ca67e10d97d98b3c0195851ebb36f4dfadd0f5
João Perdro Pires <joaopedropires61@gmail.com>
2023-07-03 22:02:41 -0300

    Adicionando primary key nas entidades

--- src/main/java/com/consumidor/projetosite/modelo/Usuario.java
+++ src/main/java/com/consumidor/projetosite/modelo/Usuario.java
@@ -1,4 +1,21 @@
 package com.consumidor.projetosite.modelo;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+
+    @Column(nullable = false)
+    private String nomeUsuario;
+
+    @Column(nullable = false)
+    private String senhaUsuario;
+
 }
